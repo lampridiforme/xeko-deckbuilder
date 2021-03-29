5c2e556dc9c2e4d4be93a8bc7e00b8b5a6c6aa5e
Christine Dai <macroavis@gmail.com>
2021-03-28 21:10:46 -0700

    build to docs folder for hosting in github pages; '

--- stencil.config.ts
+++ stencil.config.ts
@@ -12,6 +12,7 @@ export const config: Config = {
       // comment the following line to disable service workers in production
       serviceWorker: null,
       baseUrl: 'https://myapp.local/',
+      dir: 'docs'
     },
   ],
 };
