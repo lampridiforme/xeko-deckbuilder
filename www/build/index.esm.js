c8d3a584fd958f61bf75ae8e023bd050daeff243
Christine Dai <macroavis@gmail.com>
2021-03-28 20:57:23 -0700

    pusing www

---
+++ www/build/index.esm.js
@@ -0,0 +1,8 @@
+import { A as ActiveRouter } from './active-router-32c1e4b5.js';
+import './match-path-760e1797.js';
+import './index-783f980a.js';
+import './location-utils-fea12957.js';
+
+function injectHistory(Component) {
+    ActiveRouter.injectProps(Component, ['history', 'location']);
+}
