c02cb614f90d27f46bea1e07dd9d81bee8a5840b
Christine Dai <macroavis@gmail.com>
2021-05-23 14:57:00 -0700

    Commit built files

--- docs/build/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { A as ActiveRouter } from './active-router-2cbaa815.js';
-import './match-path-760e1797.js';
-import './index-37ada624.js';
-import './location-utils-fea12957.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
